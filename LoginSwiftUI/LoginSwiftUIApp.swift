72817e2d83248348777cde20a3c40dab39d200c4
Hendhy <m.hendhy.s@gmail.com>
2023-02-11 05:14:40 +0700

    Initial Commit

---
+++ LoginSwiftUI/LoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIApp.swift
+//  LoginSwiftUI
+//
+//  Created by macbook on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
